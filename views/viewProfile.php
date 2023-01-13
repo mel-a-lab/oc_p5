4a041d19a8b9000836a4cc8a45bd2ce74300adaf
mel-a-lab <melanie.dussenne@gmail.com>
2023-01-13 10:53:21 +0100

    style_title_page

--- views/viewProfile.php
+++ views/viewProfile.php
@@ -1,74 +1,74 @@
-<section class="comment-section">
-	<div class="container">
-	<h3><a href="user&profile"><b>Profil Personnel</b></a></h3>
-        <?php if (isset($success)) { ?>
-		<div class="row">
-			<div class="col-lg-3 col-md-0"></div>
-			<div class="col-lg-6 col-md-12" style="margin-bottom: -30px">
-				<div class="comment-form center-text">
-					<div class="alert-info" role="alert"><?= $success ?> </div>
+<h3 class="form-control"><b>Profil Personnel</b></h3>
+	<section class="comment-section">
+		<div class="container">
+			<?php if (isset($success)) { ?>
+			<div class="row">
+				<div class="col-lg-3 col-md-0"></div>
+				<div class="col-lg-6 col-md-12" style="margin-bottom: -30px">
+					<div class="comment-form center-text">
+						<div class="alert-info" role="alert"><?= $success ?> </div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<?php } ?>
-		<div class="row">
-			<div class="col-lg-2 col-md-0"></div>
-			<div class="col-lg-8 col-md-12">
-				<div class="comment-form">
-					<form method="POST" action="user&userEditName=action" class="contact1-form validate-form" enctype="multipart/form-data">
-						<div class="row">
-							<div class="col-sm-6">
-                                <input type="hidden" name="id" value=<?= $_SESSION['id'] ?>>
-								<input type="text" aria-required="true" minlength="3" name="firstName" class="form-control"
-                                    value=<?= $user['firstName'] ?> aria-invalid="true" required>
-							</div>
-							<div class="col-sm-6">
-								<input type="text" aria-required="true" minlength="3" name="lastName" class="form-control"
-                                    value=<?= $user['lastName'] ?> aria-invalid="true" required>
-							</div>
-							<div class="col-sm-2">
-								<div class="left-area" style="height: 70px;width: 70px;border-radius: 100px;overflow: hidden;position: absolute;top: 60%;margin-top: -35px;border: 6px solid #fff;box-shadow: 0px 0px 5px rgb(0 0 0 / 30%);">
-									<a class="avatar" href="#"><img src=<?= isset($user['picture']) ? "public/uploads/".$user['picture'] : "public/images/default-profile.png" ?> alt="Photo de profil"></a>
+			<?php } ?>
+			<div class="row">
+				<div class="col-lg-2 col-md-0"></div>
+				<div class="col-lg-8 col-md-12">
+					<div class="comment-form">
+						<form method="POST" action="user&userEditName=action" class="contact1-form validate-form" enctype="multipart/form-data">
+							<div class="row">
+								<div class="col-sm-6">
+									<input type="hidden" name="id" value=<?= $_SESSION['id'] ?>>
+									<input type="text" aria-required="true" minlength="3" name="firstName" class="form-control"
+										value=<?= $user['firstName'] ?> aria-invalid="true" required>
+								</div>
+								<div class="col-sm-6">
+									<input type="text" aria-required="true" minlength="3" name="lastName" class="form-control"
+										value=<?= $user['lastName'] ?> aria-invalid="true" required>
+								</div>
+								<div class="col-sm-2">
+									<div class="left-area" style="height: 70px;width: 70px;border-radius: 100px;overflow: hidden;position: absolute;top: 60%;margin-top: -35px;border: 6px solid #fff;box-shadow: 0px 0px 5px rgb(0 0 0 / 30%);">
+										<a class="avatar" href="#"><img src=<?= isset($user['picture']) ? "public/uploads/".$user['picture'] : "public/images/default-profile.png" ?> alt="Photo de profil"></a>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<input type="hidden" name="MAX_FILE_SIZE" value="512000" />
+									<input type="file" name="userPicture" value=<?= $user['picture'] ?>>
+								</div>
+								<div class="col-sm-12 center-text"><br>
+									<button class="submit-btn" type="submit" id="form-submit"><b>MODIFIER</b></button>
 								</div>
 							</div>
-                            <div class="col-sm-6">
-                                <input type="hidden" name="MAX_FILE_SIZE" value="512000" />
-								<input type="file" name="userPicture" value=<?= $user['picture'] ?>>
-							</div>
-							<div class="col-sm-12 center-text"><br>
-								<button class="submit-btn" type="submit" id="form-submit"><b>MODIFIER</b></button>
-							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<section class="comment-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-2 col-md-0"></div>
-			<div class="col-lg-8 col-md-12">
-				<div class="comment-form">
-					<form method="post" action="user&userEditPassword=action" class="contact1-form validate-form">
-						<div class="row">
-							<div class="col-sm-6">
-                                <input type="hidden" name="id" value=<?= $_SESSION['id'] ?>>
-								<input type="password" aria-required="true" minlength="6" name="password" class="form-control"
-									placeholder="Entrez votre mot de passe" aria-invalid="true" required>
+	</section>
+	<section class="comment-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-0"></div>
+				<div class="col-lg-8 col-md-12">
+					<div class="comment-form">
+						<form method="post" action="user&userEditPassword=action" class="contact1-form validate-form">
+							<div class="row">
+								<div class="col-sm-6">
+									<input type="hidden" name="id" value=<?= $_SESSION['id'] ?>>
+									<input type="password" aria-required="true" minlength="6" name="password" class="form-control"
+										placeholder="Entrez votre mot de passe" aria-invalid="true" required>
+								</div>
+								<div class="col-sm-6">
+									<input type="password" aria-required="true" minlength="6" name="confirmPassword" class="form-control"
+										placeholder="Confirmation du mot de passe" aria-invalid="true" required>
+								</div>
+								<div class="col-sm-12 center-text">
+									<button class="submit-btn" type="submit" id="form-submit"><b>MODIFIER</b></button>
+								</div>
 							</div>
-							<div class="col-sm-6">
-								<input type="password" aria-required="true" minlength="6" name="confirmPassword" class="form-control"
-									placeholder="Confirmation du mot de passe" aria-invalid="true" required>
-							</div>
-							<div class="col-sm-12 center-text">
-								<button class="submit-btn" type="submit" id="form-submit"><b>MODIFIER</b></button>
-							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
--- views/viewRegister.php
+++ views/viewRegister.php
@@ -1,41 +1,41 @@
-<section class="comment-section">
-<h3><a href="user&register"><b>S'inscrire</b></a></h3>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-2 col-md-0"></div>
-			<div class="col-lg-8 col-md-12">
-				<div class="comment-form">
-					<form method="post" action="user&userRegister=action" class="contact1-form validate-form">
-						<div class="row">
-							<div class="col-sm-6">
-								<input type="text" aria-required="true" minlength="3" name="firstName" class="form-control"
-									placeholder="Entrez votre prénom" aria-invalid="true" required>
+<h3 class="form-control"><b>S'inscrire</b></h3>
+	<section class="comment-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-0"></div>
+				<div class="col-lg-8 col-md-12">
+					<div class="comment-form">
+						<form method="post" action="user&userRegister=action" class="contact1-form validate-form">
+							<div class="row">
+								<div class="col-sm-6">
+									<input type="text" aria-required="true" minlength="3" name="firstName" class="form-control"
+										placeholder="Entrez votre prénom" aria-invalid="true" required>
+								</div>
+								<div class="col-sm-6">
+									<input type="text" aria-required="true" minlength="3" name="lastName" class="form-control"
+										placeholder="Entrez votre nom" aria-invalid="true" required>
+								</div>
+								<div class="col-sm-12">
+									<input type="email" aria-required="true" name="email" class="form-control"
+										placeholder="Entrez votre email" aria-invalid="true" required>
+										<b class="text-danger"><?php if (isset($errors['email'])) echo htmlspecialchars($errors['email']) ?></b>
+								</div>
+								<div class="col-sm-6">
+									<input type="password" aria-required="true" minlength="6" name="password" class="form-control"
+										placeholder="Entrez votre mot de passe" aria-invalid="true" required>
+									<b class="text-danger gras"><?php if (isset($errors['password'])) echo htmlspecialchars($errors['password']) ?></b>
+								</div>
+								<div class="col-sm-6">
+									<input type="password" aria-required="true" minlength="6" name="confirmPassword" class="form-control"
+										placeholder="Confirmation du mot de passe" aria-invalid="true" required>
+								</div>
+								<div class="col-sm-12 center-text">
+									<button class="submit-btn" type="submit" id="form-submit"><b>INSCRIPTION</b></button>
+								</div>
 							</div>
-							<div class="col-sm-6">
-								<input type="text" aria-required="true" minlength="3" name="lastName" class="form-control"
-									placeholder="Entrez votre nom" aria-invalid="true" required>
-							</div>
-							<div class="col-sm-12">
-								<input type="email" aria-required="true" name="email" class="form-control"
-									placeholder="Entrez votre email" aria-invalid="true" required>
-									<b class="text-danger"><?php if (isset($errors['email'])) echo htmlspecialchars($errors['email']) ?></b>
-							</div>
-							<div class="col-sm-6">
-								<input type="password" aria-required="true" minlength="6" name="password" class="form-control"
-									placeholder="Entrez votre mot de passe" aria-invalid="true" required>
-								<b class="text-danger gras"><?php if (isset($errors['password'])) echo htmlspecialchars($errors['password']) ?></b>
-							</div>
-							<div class="col-sm-6">
-								<input type="password" aria-required="true" minlength="6" name="confirmPassword" class="form-control"
-									placeholder="Confirmation du mot de passe" aria-invalid="true" required>
-							</div>
-							<div class="col-sm-12 center-text">
-								<button class="submit-btn" type="submit" id="form-submit"><b>INSCRIPTION</b></button>
-							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
